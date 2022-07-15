1887f2772217339e286f8e9c75d700d332ecbadb
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-07-15 11:23:34 -0500

    curso angular | Configurando las rutas routerLink y routerLinkActive, añadiendo estilos de animacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+//Servicios
+
+//Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -16,7 +23,8 @@ import { AboutComponent } from './components/about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
